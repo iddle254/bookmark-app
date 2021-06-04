8d6d2d8e5b731d9b1d584f6eebed60a549f3c737
Moss <charlesiddle99@gmail.com>
2021-06-04 13:20:58 +0300

    installed laravel ui

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Charles Motaroki',
+            'email' => 'charles@motaroki.com',
+            'password' => \bcrypt('password'),
+        ]);
     }
 }
